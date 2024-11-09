35ad95ba7c2157edcf4f1054b346fed2d12d3c82
pranjaljain45 <pranjaljain0115@gmail.com>
2024-11-09 15:05:14 +0530

    Merge Sort

--- Sorting/Recursive Sorting/MergeSort.java
+++ Sorting/Recursive Sorting/MergeSort.java
@@ -1,5 +1,121 @@
+//divide + merge
+/* divide the array into the smallest single element then merge them
+* 
+* suppose array of 5 elements
+* isko do part me divide karenge lets 3(left half) and 2(right half)
+* left half 3 ko 2 & 1
+* phir 2 ko 1 & 1
+* 
+* right half 2 ko 1 & 1
+* 
+* phir last divide krne ke baad sare ek INDIVIDUAL ARRAY me convert ho jayenge
+* aur division me HYPOTHETICAL ARRAY banegi
+* phir unhe COMPARE karenge and MERGE karenge
+* 
+* SORT LEFT AND RIGHT HALF
+* upper se neevhe tak divide neeche se upper tak merge
+*/
+
+// NOTE
+// odd no of elements ke case me array ko sabse phle jis trh se divide karenge vaise hi aage karenge
+
+// AFTER RIGHT AND LEFT ARRAY GET SORTED
+/*TWO POINTER APPROACH
+* ek pointer on left array and another on right array
+* then compare each idx element and PUT them in NEW ARRAY OF PREV UNSORTED ARRAY SIZE
+* AGAR kisi part ke element nahi reh gye toh dusri array ke sare elements as it is arrange ho jayenge
+*/
+
+
 import java.util.*;
 
 public class MergeSort {
-    
+
+    public static void merge(int arr[], int low, int mid, int high) {
+
+        ArrayList<Integer> temp = new ArrayList<>(); // temporary store all elements in sorted order
+        int left = low; // LEFT array ka first idx
+        int right = mid + 1; // RIGHT array ka first idx
+
+        // merging
+
+        // mid is the LAST IDX of LEFT array
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+
+        // jab LEFT array me elements reh jaye
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        // jab RIGHT array me elements reh jaye
+
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        // TEMP ARRAY KE ELEMNTS KO ARRAY ME SHIFT KRNA
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+    }
+
+    // har bar COMPLETE ARRAY PASS hogi
+
+    /*
+     * LOW = phle idx hypothetical array
+     * HIGH = last idx hai hypothetical array
+     */
+
+    public static void divideArray(int arr[], int low, int high) {
+
+        if (low >= high) { // jab signle element array reh jayegi tbh uska idx same hoga
+            return;
+        }
+
+        int mid = (low + high) / 2; // kis idx se array divide hogi
+
+        divideArray(arr, low, mid); // left array
+        divideArray(arr, mid + 1, high); // right array
+        merge(arr, low, mid, high); // merge sorted array
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        // eneter array
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println();
+
+        // calling divide function
+        divideArray(arr, 0, n - 1);
+
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+
+        sc.close();
+
+    }
 }
+
+
