6f27fdda2f5cd065a10037d09675e73b050427f2
pranjaljain45 <pranjaljain0115@gmail.com>
2024-11-09 13:03:42 +0530

    Bubble Sort

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,43 @@
+/*
+ * push MAX ele at the end of the array by doing ADJACENT SWAP
+ * time complexity : O(n^2)
+ * 
+ * if array sorted : time complexity : O(n) kyuki if wali condition work hi nahi karegi
+ */
+
+import java.util.*;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // enter num of elemnts
+        int arr[] = new int[n]; // reate an array
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // BUBBLE SORT
+
+        // har barr LAST ele apni correct pos pr ja rha hai isliye loop wha tak nahi
+        // jayegi
+
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // print array
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+
+        sc.close();
+    }
+}
