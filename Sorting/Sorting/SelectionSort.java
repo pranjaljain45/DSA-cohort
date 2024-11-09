67496e6e52c0cb888580f1138f07af0ef2f6727b
pranjaljain45 <pranjaljain0115@gmail.com>
2024-11-09 12:46:33 +0530

    Add files via upload

---
+++ Sorting/Sorting/SelectionSort.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // enter num of elemnts
+        int arr[] = new int[n]; // reate an array
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+        // print array
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+
+        sc.close();
+    }
+}
