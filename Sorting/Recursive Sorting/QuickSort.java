4d430c407c39f7c26e5075c4ce7cbbd2b7fcddfd
pranjaljain45 <pranjaljain0115@gmail.com>
2024-11-09 18:06:48 +0530

    sorting

--- Sorting/Recursive Sorting/QuickSort.java
+++ Sorting/Recursive Sorting/QuickSort.java
@@ -41,6 +41,82 @@
  */
 
 
+import java.util.*;
+
+public class QuickSort {
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (arr[i] <= pivot && i <= high - 1) { // phle bda element from start array
+                i++;
+            }
+
+            while (arr[j] > pivot && j >= low + 1) { // phle chota element from end array
+                j--;
+            }
+
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // Place the pivot in the correct position
+
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+
+        return j;
+    }
+
+    public static void sort(int[] arr, int low, int high) {
+
+        if (low < high) {
+            int partitionIdx = partition(arr, low, high);
+
+            sort(arr, low, partitionIdx - 1);
+            sort(arr, partitionIdx + 1, high);
+        }
+    }
+
+    public static int[] qs(int[] arr) {
+
+        sort(arr, 0, arr.length - 1);
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        // enter array
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println();
+
+        // call function
+        qs(arr);
+
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+
+        sc.close();
+
+    }
+}
 
 
 /*
