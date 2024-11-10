8613cac98c5b9e45b277ca9b1637ec37dbb8a732
pranjaljain45 <pranjaljain0115@gmail.com>
2024-11-10 18:00:01 +0530

    Print a num in decreasing order

---
+++ Recursion and backtracking/PrintDecreasing.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class printDecreasing {
+
+    static void printElements(int n) {
+        if(n == 0){
+            return;
+        }
+        System.out.println(n);
+        printElements(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        printElements(n);
+
+        sc.close();
+    }
+}
