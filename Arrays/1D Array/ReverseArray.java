2712b2341bac37176eb91fad7c29fc3886e16384
pranjaljain45 <pranjaljain0115@gmail.com>
2024-11-10 10:13:34 +0530

    Reverse of an array

---
+++ Arrays/1D Array/ReverseArray.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class reverseArray {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+
+        sc.close();
+    }
+}
