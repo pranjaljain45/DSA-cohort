321488fbbc869d759caf8acd125bd7c9bbb9fed2
pranjaljain45 <pranjaljain0115@gmail.com>
2024-11-12 04:15:51 +0530

    Print path horizontal  & vertical path

---
+++ Recursion and backtracking/GetMazepath.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class getMazepath {
+
+    // sc = start col, sr = start row
+    // dc = destination col, dr = destination row
+
+    static ArrayList<String> findPath(int sr, int sc, int dr, int dc) {
+        if (sr == dr && sc == dc) {
+            ArrayList<String> bres = new ArrayList<>();
+            bres.add("");
+            return bres;
+        }
+
+        // horizontal path
+        ArrayList<String> hp = new ArrayList<>();
+
+        // vertical path
+        ArrayList<String> vp = new ArrayList<>();
+
+        if (sc < dc) {
+            hp = findPath(sr, sc + 1, dr, dc);
+        }
+
+        if (sr < dr) {
+            vp = findPath(sr + 1, sc, dr, dc);
+        }
+
+        ArrayList<String> paths = new ArrayList<>();
+
+        for (String hpath : hp) {
+            paths.add("h" + hpath);
+        }
+        for (String vpath : vp) {
+            paths.add("v" + vpath);
+        }
+
+        return paths;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        ArrayList<String> st = findPath(1, 1, m, n);
+
+        for (String s : st) {
+            System.out.println(s);
+        }
+
+        sc.close();
+    }
+}
