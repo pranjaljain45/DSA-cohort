29b8acb153506694d16c99581eb851bc17004947
pranjaljain45 <pranjaljain0115@gmail.com>
2024-11-12 02:57:19 +0530

    Print subsets of a string

---
+++ Recursion and backtracking/GetSubsequence.java
@@ -0,0 +1,47 @@
+/*Subset calculate krte hai string ka BUT string ki length undefined hoti hai isliye Arraylist ka use krte hai store krne ke liye values */
+
+import java.util.*;
+
+public class getSubsequence {
+
+    static ArrayList<String> subsetString(String s) {
+
+        if (s.length() == 0) {
+
+            ArrayList<String> bres = new ArrayList<>();
+            bres.add(""); // ek empty subset bhi hota hai
+            return bres;
+        }
+
+        char ch = s.charAt(0); // har baar string ka ek ek char alag karenge
+
+        String restStr = s.substring(1);// bki string ko alag store
+
+        ArrayList<String> addString = subsetString(restStr); // phir se call kia taki chars ko alag kr ske
+
+        ArrayList<String> result = new ArrayList<>(); // final ans ko store karegi
+
+        for (String str : addString) {
+            result.add(str);
+            result.add(ch + str);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine(); // enter string
+
+        ArrayList<String> ss = subsetString(str); // array return hogi subsets ki string nahi
+
+        // print string subsets
+
+        for (String s : ss) {
+            System.out.println(s);
+        }
+
+        sc.close();
+    }
+}
