b5a70e6c80d16d8f468d7485461f4d8c934b678a
Pranjal Jain <pranjaljain0115@gmail.com>
2024-11-14 22:47:47 +0530

    Print path using horizontal & vertical steps

--- Recursion and backtracking/GetMazepath.java
+++ Recursion and backtracking/GetMazepath.java
@@ -46,10 +46,8 @@ public class getMazepath {
 
         ArrayList<String> st = findPath(1, 1, m, n);
 
-        for (String s : st) {
-            System.out.println(s);
-        }
-
+            System.out.println(st);
+      
         sc.close();
     }
 }
