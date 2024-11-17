2d7be22c798470a89bc904ac7a296b375bd1929b
Pranjal Jain <pranjaljain0115@gmail.com>
2024-11-17 19:00:36 +0530

    Solve and convert post expression to prefix and infix

---
+++ Stack/PostEvaluationConversion.java
@@ -0,0 +1,83 @@
+/*
+ * solve from left to right
+ */
+
+import java.util.*;
+
+public class postEvaluationConversion {
+   public static void main(String[] args) {
+     
+      Scanner sc = new Scanner(System.in);
+
+      String str = sc.nextLine();
+
+      Stack<Integer> value = new Stack<>();
+
+      Stack<String> infix = new Stack<>();
+      Stack<String> prefix = new Stack<>();
+
+      for (char ch : str.toCharArray()) {
+        
+         if (Character.isDigit(ch)) {
+            value.push(ch - '0');
+         } else if (Character.isLetter(ch)) {
+            value.push(0);
+         } else {
+            solve(value, ch);
+         }
+      }
+     
+      System.out.println(value.pop());
+
+      // conversion
+
+      for (char ch : str.toCharArray()) {
+
+         if (Character.isLetterOrDigit(ch)) {
+            String s = Character.toString(ch);
+            infix.push(s);
+            prefix.push(s);
+         } 
+         else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+            convert(infix, prefix, ch);
+         }
+      }
+
+      System.out.println(prefix.pop());
+      System.out.println(infix.pop());
+
+      sc.close();
+   }
+
+   static void solve(Stack<Integer> value, char ch) {
+
+      int v2 = value.pop();
+      int v1 = value.pop();
+
+      if (ch == '+') {
+         value.push(v1 + v2);
+      } else if (ch == '-') {
+         value.push(v1 - v2);
+      } else if (ch == '*') {
+         value.push(v1 * v2);
+      } else if (ch == '/') {
+         value.push(v1 / v2);
+      } else {
+         value.push(0);
+      }
+   }
+
+   static void convert(Stack<String> infix, Stack<String> prefix, char ch) {
+     
+      String iv2 = infix.pop();
+      String iv1 = infix.pop();
+
+      infix.push("(" + iv1 + ch + iv2 + ")");
+
+      String pv2 = prefix.pop();
+      String pv1 = prefix.pop();
+
+      prefix.push("(" + ch + pv1 + pv2 + ")");
+
+   }
+}
