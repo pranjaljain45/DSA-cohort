ee798a390e3e1e34741d6c23efab4b59467e9e69
Pranjal Jain <pranjaljain0115@gmail.com>
2024-11-19 22:48:06 +0530

    Basic of Oops

---
+++ Oops/StartwithOops.java
@@ -0,0 +1,21 @@
+class : kind of fn jisme data members or fns dono hote hai 
+
+  eg public static person{
+  int age;
+
+  void printAge(){
+    Syso(age);
+  }
+}
+
+void main(){
+  person p1 = new person();
+  p1.age = 10;
+  p1.printAge();
+}
+
+
+objects : heap me jo instance banta hai jha data memebers or value store hoti hai 
+      eg p1
+
+stack : jo object declare kis hai heap me uska reference (4k, 5k...)
