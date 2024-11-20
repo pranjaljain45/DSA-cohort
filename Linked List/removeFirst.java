81bea44aac4d2a4673afec6772a7827a0178125c
Pranjal Jain <pranjaljain0115@gmail.com>
2024-11-20 18:32:33 +0530

    Remove the first element of list

---
+++ Linked List/removeFirst.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+public class removeFirst {
+
+    public static class Node {
+        int data;
+        Node next;
+    }
+
+    public static class linkedList {
+        Node head;
+        Node tail;
+
+        int size;
+
+        void addEnd(int val) {
+
+            Node temp = new Node(); // create a new node
+            temp.data = val;
+            temp.next = null;
+
+            if (size == 0) {
+                head = tail = temp;
+            } else {
+                tail.next = temp; // prev last node me new add note ka address
+                tail = temp; // aur last node
+            }
+            size++;
+        }
+
+        void printList() {
+            Node temp = head;
+
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+
+                temp = temp.next; // abh prev store node me store address wali node
+            }
+        }
+
+        void removefirst() {
+
+            if (size == 0) {
+                System.out.print("list is empty");
+            } else if (size == 1) {
+                head = tail = null;
+                size--;
+            } else {
+                head = head.next;
+                size--;
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        linkedList list1 = new linkedList();
+
+        list1.addEnd(20);
+        list1.addEnd(30);
+        list1.addEnd(40);
+        list1.addEnd(50);
+        list1.addEnd(60);
+
+        list1.printList();
+
+        list1.removefirst();
+        list1.removefirst();
+
+        list1.printList();
+
+        sc.close();
+    }
+}
