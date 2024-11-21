c6f3f5b867d974fbb208a49adecfa945b1419ecf
Pranjal Jain <pranjaljain0115@gmail.com>
2024-11-21 19:53:10 +0530

    Reverse a linked list using pointer appraoch

---
+++ Linked List/reverseLinkedListPointerAppraoch.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+public class reverseLinkedList {
+
+    public static class Node {
+        int data;
+        Node next;
+    }
+
+    public static class linkedList {
+        Node head;
+        Node tail;
+
+        int size;
+
+        void addEnd(int val) {
+
+            Node temp = new Node(); // create a new node
+            temp.data = val;
+            temp.next = null;
+
+            if (size == 0) {
+                head = tail = temp;
+            } else {
+                tail.next = temp; // prev last node me new add note ka address
+                tail = temp; // aur last node
+            }
+            size++;
+        }
+
+        void printList() {
+            Node temp = head;
+
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+
+                temp = temp.next; // abh prev store node me store address wali node
+            }
+        }
+
+        void swapNode() {
+            Node prev = null;
+            Node curr = head;
+
+            while (curr != null) {
+                Node currNode = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = currNode;
+            }
+
+            Node temp = head;
+            head = tail;
+            tail = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        linkedList list1 = new linkedList();
+
+        list1.addEnd(20);
+        list1.addEnd(30);
+        list1.addEnd(40);
+        list1.addEnd(50);
+
+        list1.swapNode();
+
+        list1.printList();
+
+        sc.close();
+    }
+}
