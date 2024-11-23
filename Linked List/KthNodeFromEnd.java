c6237c5a1487b0de6b5585c2331b2aa4a5bc028c
Pranjal Jain <pranjaljain0115@gmail.com>
2024-11-23 17:23:09 +0530

    Find Kth node from end

---
+++ Linked List/KthNodeFromEnd.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class KthNodeFromEnd {
+
+    public static class Node {
+        int data;
+        Node next;
+    }
+
+    public static class Kth {
+
+        Node head;
+        Node tail;
+        int size;
+
+        void addValues(int val) {
+            Node temp = new Node();
+            temp.data = val;
+            temp.next = null;
+
+            if (size == 0) {
+                head = tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
+
+        int getKthValue(int k) {
+            Node slow = head;
+            Node fast = head;
+
+            for (int i = 0; i < k; i++) {
+                fast = fast.next;
+            }
+
+            while (fast != tail) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+
+            return slow.data;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Kth list = new Kth();
+
+        list.addValues(2);
+        list.addValues(3);
+        list.addValues(4);
+        list.addValues(5);
+
+        System.out.println(list.getKthValue(0));
+
+        sc.close();
+    }
+}
