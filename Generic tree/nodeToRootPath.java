215dcc9eae3a57ec8d98af2ec272552a1351fe84
Pranjal Jain <pranjaljain0115@gmail.com>
2024-12-03 18:14:58 +0530

    Find node to root path

---
+++ Generic tree/nodeToRootPath.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class genricTree {
+
+    public static class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
+    public static ArrayList<Integer> nodeTorootPath(Node node, int data) {
+
+        if (node.data == data) {
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(node.data);
+            return list;
+        }
+
+        for (Node child : node.children) {
+            ArrayList<Integer> list2 = nodeTorootPath(child, data);
+            if (list2.size() > 0) {
+                list2.add(node.data);
+                return list2;
+            }
+        }
+
+        return new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+
+        Node root = null;
+
+        int[] arr = { 1, 2, 4, -1, 5, 8, -1, 9, -1, -1, -1, 3, 6, -1, 7, 10, -1, -1, -1, -1 };
+
+        Stack<Node> st = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == -1) {
+                st.pop();
+
+            } else {
+                Node temp = new Node();
+                temp.data = arr[i];
+
+                if (st.size() > 0) {
+                    st.peek().children.add(temp);
+                } else {
+                    root = temp;
+                }
+
+                st.push(temp);
+            }
+        }
+
+        System.out.println(nodeTorootPath(root, 10));
+    }
+}
