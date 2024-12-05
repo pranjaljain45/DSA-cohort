e65658f3e63db5b330ee17ecf46aba89b5eeefd2
Pranjal Jain <pranjaljain0115@gmail.com>
2024-12-06 02:54:45 +0530

    reverse rows of a matrix

--- BASICS/reverseRowMatrix.java
+++ BASICS/reverseRowMatrix.java
@@ -3,10 +3,13 @@ n == matrix length
 
 
 for (int i = 0; i < n; i++) {
+            
             for (int j = 0; j < n / 2; j++) {
 
                 int temp = matrix[i][j];
+            
                 matrix[i][j] = matrix[i][n - 1 - j];
+                
                 matrix[i][n - 1 - j] = temp;
 
             }
